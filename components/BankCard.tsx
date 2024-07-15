f2d570b9c4c5a8d0c2dc34afa8d406309f19ed11
Dea Sharma <deasharma@gmail.com>
2024-07-15 15:29:16 +0530

    changes in left sidebar

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,4 +1,5 @@
 import { formatAmount } from '@/lib/utils';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -20,15 +21,24 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
           <article className="flex flex-col gap-2">
             <div className="flex justify-between">
               <h1 className="text-12 font-semibold text-white">{userName}</h1>
-              <h2 className="text-12 font-semibold text-white">**</h2>
+              <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              **** **** **** <span className="text-16">$ {account.mask}</span>
+             ●●●● ●●●● ●●●● <span className="text-16"> ₹ {1234}</span>
             </p>
           </article>
 
         </div>
+
+        <div className="bank-card_icon">
+          <Image src="/icons/Paypass.svg" width={20} height={24} alt="pay" />
+          <Image src="/icons/mastercard.svg" width={45} height={32} alt="mastercard" className="ml-5" />
+        </div>
+
+        <Image src="/icons/lines.png" width={316} height={190} alt="lines" className="absolute top-0 left-0" />
       </Link>
+
+      {/* COPY */}
     </div>
   );
 };
