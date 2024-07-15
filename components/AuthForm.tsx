12a958ffe7af334f93e082aa022a215949c46cd0
Dea Sharma <deasharma@gmail.com>
2024-07-16 02:10:25 +0530

    sign in and sign up forms

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,13 +18,17 @@ import {
 } from "@/components/ui/form";
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
+import { Loader2 } from 'lucide-react';
 
 const AuthForm = ({ type }: { type: string }) => {
     const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const formSchema = authFormSchema(type);
 
     // Define your form.
-    const form = useForm<z.infer<typeof authFormSchema>>({
-        resolver: zodResolver(authFormSchema),
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
         defaultValues: {
             email: "",
             password: ''
@@ -32,10 +36,24 @@ const AuthForm = ({ type }: { type: string }) => {
     });
 
     // Define a submit handler.
-    function onSubmit(values: z.infer<typeof authFormSchema>) {
-        // Do something with the form values.
-        // This will be type-safe and validated.
-        console.log(values);
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
+        setIsLoading(true);
+        try {
+            if (type === 'sign-up') {
+                const userData = {
+                    firstName: data.firstName
+                }
+                // Handle sign-up logic here
+            }
+
+            if (type === 'sign-in') {
+                // Handle sign-in logic here
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     return (
@@ -63,11 +81,49 @@ const AuthForm = ({ type }: { type: string }) => {
                 <>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                            {type === 'sign-up' && (
+                                <>
+                                    <div className="flex gap-4">
+                                        <CustomInput control={form.control} name='firstName' label="First Name" placeholder='Enter your first name' />
+                                        <CustomInput control={form.control} name='lastName' label="Last Name" placeholder='Enter your last name' />
+                                    </div>
+
+                                    <CustomInput control={form.control} name='address' label="Address" placeholder='Enter your address' />
+                                    <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
+
+                                    <div className="flex gap-4">
+                                        <CustomInput control={form.control} name='state' label="State" placeholder='Example: Delhi' />
+                                        <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 110011' />
+                                    </div>
+                                    <div className="flex gap-4">
+                                        <CustomInput control={form.control} name='dob' label="Date of Birth" placeholder='dd-mm-yyyy' />
+                                        <CustomInput control={form.control} name='aadhar' label="Aadhar Card Number " placeholder='**** **** ****' />
+                                    </div>
+                                </>
+                            )}
+
                             <CustomInput control={form.control} name='email' label="Email" placeholder='Enter your email' />
                             <CustomInput control={form.control} name='password' label="Password" placeholder='Enter your password' />
-                            <Button type="submit">Submit</Button>
+
+                            <div className='flex flex-col gap-4'>
+                                <Button type="submit" disabled={isLoading} className="form-btn">
+                                    {isLoading ? (
+                                        <>
+                                            <Loader2 size={20} className="animate-spin" /> &nbsp; Loading...
+                                        </>
+                                    ) : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+                                </Button>
+                            </div>
+
                         </form>
                     </Form>
+
+                    <footer className='flex justify-center gap-1'>
+                        <p className='text-14 font-normal text-gray-600'>{type === 'sign-in' ? "Don't have an account? " : "Already have an account?"}</p>
+                        <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-link'>
+                            {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+                        </Link>
+                    </footer>
                 </>
             )}
         </section>
@@ -75,4 +131,3 @@ const AuthForm = ({ type }: { type: string }) => {
 }
 
 export default AuthForm;
-
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -13,7 +14,7 @@ export const formatDateTime = (dateString: Date) => {
     weekday: "short", // abbreviated weekday name (e.g., 'Mon')
     month: "short", // abbreviated month name (e.g., 'Oct')
     day: "numeric", // numeric day of the month (e.g., '25')
-    hour: "numeric", // numeric hour (e.g., '8')
+    hour: "numeric", // numeric hour (e.g., '3')
     minute: "numeric", // numeric minute (e.g., '30')
     hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
   };
@@ -32,7 +33,7 @@ export const formatDateTime = (dateString: Date) => {
   };
 
   const timeOptions: Intl.DateTimeFormatOptions = {
-    hour: "numeric", // numeric hour (e.g., '8')
+    hour: "numeric", // numeric hour (e.g., '3')
     minute: "numeric", // numeric minute (e.g., '30')
     hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
   };
@@ -193,3 +194,17 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+export const authFormSchema = (type: string) => z.object({
+ //sign up
+  firstName: type === 'sign-in' ? z.string().optional() :  z.string().min(3),
+  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  address:type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  state:type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  postalCode:type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  dob:type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  aadhar:type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  // both
+  email: z.string().email(),
+  password: z.string().min(8),
+})
